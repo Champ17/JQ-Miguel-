02c24c4d9e97d68083d469a3a802c7cec7975db0
Miguel Banuelos <mistergengar@gmail.com>
2015-01-09 09:19:37 -0800

    milestone 3

--- public_html/scripts.js
+++ public_html/scripts.js
@@ -3,7 +3,17 @@ $("document").ready(function() {
     $(".k").css("text-align", "center");
     $('p:first').css("color", "black");
     $("p:last").css("color", "blue");
-    $('body').css("background-image", "url(http://www.psdgraphics.com/file/colorful-triangles-background.jpg)");
+    $('body').css("background-image", "url(http://1.media.dorkly.cvcdn.com/55/71/c988305d1cae5e1f56b2178a1c43b74d.gif)");
     $("body").css("background-size", "cover");
-    $("").css("");
+    $("h1").bind("mouseover", mouseOverMe);
+    $("h1").bind("mouseout", mouseOutMe);
 });
+
+function mouseOverMe() {
+    $("h1").css("color", "red");
+}
+function mouseOutMe(){
+    
+}
+
+
